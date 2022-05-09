49a4801705df854b653fd3bf6d13e1740f7ac24d
DESKTOP-LO3FN3B\TitoI <titoiriepal@gmail.com>
2022-05-09 15:52:53 +0200

    header mobile

--- views/nav/index.php
+++ views/nav/index.php
@@ -1,1 +1,3 @@
+<?php  include_once __DIR__ . '/../templates/header.php'; ?>
+
 <h1>Desde Indice</h1>
---
+++ views/templates/header.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="contenedor backimg">
+        <div class="title-header">
+            <div class="box-header">
+                <pre><h1>IV  pasión<br>       viviente <br>           Iriépal</h1></pre>
+                <p><i class="fa-solid fa-bars menu-bars"></i></p>
+            </div>
+
+            <nav class="menu-principal">
+                <a href="#" class="boton">noticias</a>
+                <a href="#" class="boton">galeria</a>
+                <a href="#" class="boton">blog</a>
+                <a href="#" class="boton">elenco</a>
+                <a href="#" class="boton">ediciones anteriores</a>
+            </nav>
+
+            <nav class="redes-sociales">
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-pinterest"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            </nav>
+
+        </div>
+
+        
+        
+    </div>
+
+        
+
+</header>
