82ce32e520e20c224520500772e035db70e738f3
titoiriepal <titoiriepal@hotmail.com>
2026-03-31 12:08:59 +0200

    Aria-label tag to social media links

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -2,11 +2,11 @@
 
     <div class="contenedor-footer">
         <nav class="redes-sociales">
-            <a href="https://www.facebook.com/pasionvivienteiriepal"><i class="fa-brands fa-facebook"></i></a>
+            <a href="https://www.facebook.com/pasionvivienteiriepal" aria-label="Acceso a la página de facebook de la pasión viviente de Iriépal"><i class="fa-brands fa-facebook"></i></a>
             <!-- <a href="#"><i class="fa-brands fa-twitter"></i></a>
             <a href="#"><i class="fa-brands fa-pinterest"></i></a>
             <a href="#"><i class="fa-brands fa-youtube"></i></a> -->
-            <a href="https://www.instagram.com/pasionvivienteiriepal/"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://www.instagram.com/pasionvivienteiriepal/" aria-label="Acceso a la página de Instagram de la pasión viviente de Iriépal"><i class="fa-brands fa-instagram"></i></a>
         </nav>
         <div class="organiza">
                 
--- views/templates/redes_sociales.php
+++ views/templates/redes_sociales.php
@@ -1,7 +1,7 @@
             <nav class="redes-sociales">
-                <a href="https://www.facebook.com/pasionvivienteiriepal"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://www.facebook.com/pasionvivienteiriepal" aria-label="Acceso a la página de Facebook de la pasión viviente de Iriépal"><i class="fa-brands fa-facebook"></i></a>
                 <!-- <a href="#"><i class="fa-brands fa-twitter"></i></a>
                 <a href="#"><i class="fa-brands fa-pinterest"></i></a>
                 <a href="#"><i class="fa-brands fa-youtube"></i></a> -->
-                <a href="https://www.instagram.com/pasionvivienteiriepal/"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.instagram.com/pasionvivienteiriepal/" aria-label="Acceso a la página de Instagram de la pasión viviente de Iriépal"><i class="fa-brands fa-instagram"></i></a>
             </nav>
